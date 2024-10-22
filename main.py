66400fbc07cff7888ea7f9dbbd51a5ce4606eaae
Singhanat Rerng-Osot <singha.osot@gmail.com>
2024-10-22 09:13:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello")
+import pandas as pd
+
+
+business_data = pd.read_csv("dbfs/my-data/data.csv")
+
+class GreatDatabricksPipeline:
+  """ A Data pipeline that will run on databricks :)
+  """
+  def __init__(self, data: pd.DataFrame) -> None:
+    self.data = data
+
+  def run(self) -> None:
+    """ Run Datatabricks pipeline method.
+    """
+    pass
+
+
+pipeline = GreatDatabricksPipeline(data = business_data)
+pipeline.run()
